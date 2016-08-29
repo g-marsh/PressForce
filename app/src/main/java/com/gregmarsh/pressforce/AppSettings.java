548e541cafba9a94291660b70a44c9ca21bc733b
marshg <iltrmb@gmail.com>
2016-08-29 16:30:48 -0500

    add app settings

---
+++ app/src/main/java/com/gregmarsh/pressforce/AppSettings.java
@@ -0,0 +1,17 @@
+package com.gregmarsh.pressforce;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by marshg on 8/29/2016.
+ */
+public class AppSettings extends AppCompatActivity {
+
+    // Constants
+    public static final String APP_SETTINGS = "AppSettings";
+    public static final String INSIDE_DIAMETER = "InsideDiameter";
+    public static final String OUTSIDE_DIAMETER = "OutsideDiameter";
+    public static final String TRANSITION_DIAMETER = "TransitionDiameter";
+    public static final String CONTACT_LENGTH = "ContactLength";
+
+}
